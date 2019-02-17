e4fd73eefb1073d9b8c8588f36bd125cc150bb64
shmuelhalbfinger <40396822+shmuelhalbfinger@users.noreply.github.com>
2019-02-16 23:37:39 -0500

    Edited the coordinates of the map

--- mapPlacement.js
+++ mapPlacement.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapID').setView([38.983270399999995, -76.9466368], 13);
+var mymap = L.map('map').setView([38.983270399999995, -76.9466368], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
