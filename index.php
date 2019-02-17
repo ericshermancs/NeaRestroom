9c3c76e2acb1a654eb708ecfd1ad0fd70cc0e1a3
ericshermancs <zensap92@gmail.com>
2019-02-17 15:05:19 -0500

    message :(

--- index.php
+++ index.php
@@ -25,6 +25,9 @@
             width: 100%;
             height: 100%;
         }
+    	.leaflet-control-container .leaflet-routing-container-hide {
+        display: none;
+    }
     </style>
 
     <?php
