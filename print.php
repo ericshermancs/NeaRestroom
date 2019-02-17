188e0121da2a961c01a259ab974042bd19a1c27f
Eli Szafranski <eliszafranski@Elis-MacBook-Air.local>
2019-02-17 16:15:29 -0500

    changed parameter of function

--- print.php
+++ print.php
@@ -1,18 +1,22 @@
 <?php
-    function find_review($get){
-    $database = file_get_contents('database.json');
-    $json = json_decode($database, true);
-    $index; 
-    for($i = 0; $i < sizeof(json); $i++){
-        if($json[$i]['unique_id'] == $get['unique_id']){
-            $index = $i; 
-            break; 
+    function find_review($unique_id){
+        $database = file_get_contents('database.json');
+        $json = json_decode($database, true);
+        $index; 
+        for($i = 0; $i < sizeof(json); $i++){
+            if($json[$i]['unique_id'] == $unique_id){
+                $index = $i; 
+                break; 
+            }
+        }
+
+        for($i = 0; $i < sizeof($json[$index]['reviews']); $i++){
+            $review = $json[$index][$i];
+            echo $review['overall_rating'].'<br>'.$reveiw['cleanliness_level'].'<br>'.$review['comments'].'<br><br>';
         }
     }
-
-    for($i = 0; $i < sizeof($json[$index]['reviews']); $i++)
-        $review = $json[$index][$i];
-        echo $review['overall_rating'].'<br>'.$reveiw['cleanliness_level'].'<br>'.$review['comments'].'<br><br>';
+    if (isset($_GET['unique_id'])){
+        find_review($_GET['unique_id']);
     }
 ?>
 
