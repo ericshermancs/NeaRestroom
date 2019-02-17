abb4bc6e413c52d1a36f9a024d075d3d9cdce93d
shmuelhalbfinger <40396822+shmuelhalbfinger@users.noreply.github.com>
2019-02-17 00:39:17 -0500

    Created a class to instantiate a new bathroom

--- mapPlacement.js
+++ mapPlacement.js
@@ -1,8 +1,24 @@
-var mymap = L.map('map').setView([38.983270399999995, -76.9466368], 13);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		id: 'mapbox.streets'
-	}).addTo(mymap);
+function mapPlacement() {
+	var mymap = L.map('map').setView([38.983270399999995, -76.9466368], 13);
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+				'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+				'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+			id: 'mapbox.streets'
+		}).addTo(mymap);
+}
+mapPlacement();
+
+function addBathroom (name, cleanliness, rating, sinks, dry, gender) {
+	br = {
+		'categories': ['handicap', 'top_stall_gap', 'bottom_stall_gap', 'breastfeeding', 'diaper_change']
+			'name': name,
+			'cleanliness_level': cleanliness
+			'overall_rating': rating,
+			'sinks': sinks,
+			'method_of_drying': dry,
+			'gender': gender,
+			'reviews': [];
+	}
+}
