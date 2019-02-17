577ef70477a9733690f64bd4e062af4ea6210c45
ericshermancs <zensap92@gmail.com>
2019-02-17 04:20:37 -0500

    fix echo, change json to one liner

--- project_functions.js
+++ project_functions.js
@@ -1,14 +1,4 @@
-function mapPlacement() {
-	var mymap = L.map('map').setView([38.983270399999995, -76.9466368], 13);
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-			maxZoom: 18,
-			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-				'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-				'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-			id: 'mapbox.streets'
-		}).addTo(mymap);
-}
-mapPlacement();
+
 /*
 function addBathroom (name, cleanliness, rating, sinks, dry, gender, latitude, longitude, unique_id) {
 	var d = new Date();
