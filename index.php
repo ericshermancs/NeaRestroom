14b00a5a2469764d559b6c0bb90fc0627a5da355
ericshermancs <zensap92@gmail.com>
2019-02-19 18:15:11 -0500

    message :(

--- index.php
+++ index.php
@@ -614,6 +614,7 @@
             console.log('L:'+preset_location);
             if(preset_location!=null){
                 window.userLat = parseFloat(preset_location.split(',')[0]);
+                console.log(preset_location.split(',')[0]);
                 window.userLat = parseFloat(preset_location.split(',')[1]);
                 //window.zoom = parseFloat(preset_zoom);
                 console.log("PRESET: "+window.userLat+","+window.userLon);
