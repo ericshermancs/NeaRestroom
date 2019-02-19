b9fe617700e835066c28d6ca4afbc120bddfff09
ericshermancs <zensap92@gmail.com>
2019-02-19 18:05:47 -0500

    implement saved location with zoom

--- index.php
+++ index.php
@@ -608,7 +608,7 @@
             */
             let preset_location = (document.cookie.match(/^(?:.*;)?\s*location\s*=\s*([^;]+)(?:.*)?$/)||[,null])[1]
             let preset_zoom = (document.cookie.match(/^(?:.*;)?\s*zoom\s*=\s*([^;]+)(?:.*)?$/)||[,null])[1]
-            if(preset_zoom==null){
+            if(preset_zoom==null || window.zoom==null){
                 window.zoom = 13;
             }
             if(preset_location!=null){
