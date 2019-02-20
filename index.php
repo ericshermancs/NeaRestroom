5bdeb38cc4f15c88d44e933e943e516c64b3184b
ericshermancs <zensap92@gmail.com>
2019-02-19 22:07:17 -0500

    fixed zoom and current location

--- index.php
+++ index.php
@@ -626,7 +626,8 @@
 
             }
 
-            var mymap = L.map('map').setView([window.userLat, window.userLon], window.zoom);
+            var mymap = L.map('map');
+
             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
                 maxZoom: 18,
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
@@ -727,6 +728,7 @@
                 bar[i].style.top = '60px';
             }
 
+            mymap.setView([window.userLat, window.userLon], window.zoom);
         }
         /*
         try{
